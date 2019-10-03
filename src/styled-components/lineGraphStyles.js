c25113159a90404fb1f13321b908a7359ad63e52
tbelk12 <taylor.belk@americannational.com>
2019-10-03 13:20:11 -0500

    created and added a wrapper to the line graph

---
+++ src/styled-components/lineGraphStyles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const LineChartWrapper = styled.div`
+    width: 70%;
+    height: auto;
+    margin: 8em auto;
+    padding: 2em;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 6px 8px 20px darkgray;
+
+    .lineChart {
+        margin: 0 auto;
+    }
+`
