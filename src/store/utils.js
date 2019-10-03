b1415b6258cc33f78ae64cb957ff61cac8ca65d6
tbelk12 <taylor.belk@americannational.com>
2019-10-03 16:17:13 -0500

    added utility methods to convert epoch timestamps

---
+++ src/store/utils.js
@@ -0,0 +1,17 @@
+function pad(date) {
+  let dateArr = date.split('');
+  if(dateArr.length >= 10) {
+    dateArr.unshift(0);
+    dateArr.join('');
+  }
+
+  return dateArr.slice(0, 5).join('');
+}
+
+export const convertEpochToLocalTime = (timestamp) => {
+    return pad(new Date(timestamp).toLocaleTimeString());
+};
+
+export const subtractMinutes = (timestamp) => {
+  return timestamp - (30*60000);
+}
