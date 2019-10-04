41ce27a4f85703b81627899ab6293253c1d4d64e
tbelk12 <taylor.belk@americannational.com>
2019-10-04 16:27:02 -0500

    added possible query strings that I may need between the components

--- src/components/queryStrings.js
+++ src/components/queryStrings.js
@@ -14,3 +14,36 @@ export const getRealTimeMeasurements = `
         }
     }
 `
+
+export const getMultipleMeasurements = `
+    query getMeasurementsQuery ($metricArray: [MeasurementQuery]) {
+        getMultipleMeasurements(input: $metricArray) {
+            metric
+            measurements {
+                metric
+                at
+                value
+                unit
+            }
+        }
+    }
+`
+
+export const getMeasurementsQuery = `
+query getMeasurements ($met: MeasurementQuery) {
+    getMeasurements(input: $met) {
+        metric
+        at
+        value
+        unit
+    }
+}
+`
+
+export const getLastValue = `
+    query getLastKnown($metricName: String!) {
+        getLastKnownMeasurement(metricName: $metricName) {
+            value
+        }
+    }
+`
