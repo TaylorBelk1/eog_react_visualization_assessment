246db87524249d317dcf944d50ec924c6bd48d28
tbelk12 <taylor.belk@americannational.com>
2019-10-04 16:29:45 -0500

    set subscription to launch when the graph gets rendered

--- src/components/line-graphs/LineGraphMain.jsx
+++ src/components/line-graphs/LineGraphMain.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import LineGraph from './LineGraph';
 import { data } from './dummy-data';
+import { useSubscription } from 'urql';
+import { connect } from 'react-redux';
+import { setMeasurements, setLoading } from '../../store/actions';
+import {
+    getRealTimeMeasurements
+} from '../queryStrings';
 
-const LineGraphMain = () => {
+const handleSub = (measurements = [], response) => {
+    return [response.data, ...measurements]
+};
+
+const LineGraphMain = (props) => {
+    const [res] = useSubscription({
+        query: getRealTimeMeasurements, handleSub
+    });
+    if(!res.data) return <p>No Data</p>
+
+    props.setMeasurements(res.data)
+
+
     return (
         <LineGraph data={data.getMeasurements} />
     )
 }
 
-export default LineGraphMain
+const mstp = state => {
+    return {
+        measurements: state.data,
+        loading: state.loading
+    }
+}
+
+export default connect(mstp, { setMeasurements, setLoading })(LineGraphMain)
