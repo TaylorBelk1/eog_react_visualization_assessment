09b39cde07103422889af853c4b50b8068044ebb
tbelk12 <taylor.belk@americannational.com>
2019-10-04 16:28:31 -0500

    started the set up for the query to get the last known value, commiting to switch pcs

--- src/components/metrics/MetricsCard.jsx
+++ src/components/metrics/MetricsCard.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { getMeasurementsQuery, getLastValue } from '../queryStrings';
+import { subtractMinutes } from '../../store/utils';
+import { useQuery } from 'urql';
 
 const useStyles = makeStyles({
   card: {
@@ -16,17 +19,58 @@ const useStyles = makeStyles({
   }
 });
 
+// this component needs to initially query getMeasurements and send it
+// to redux so it will have a baseline set of data
+
 const MetricsCard = (props) => {
     const classes = useStyles();
+    const [toggleChart, setToggleChart] = useState(false);
+    const [cardData, setCardData] = useState(null);
+    const metricName = props.title;
+
+    const [{ fetching, data, error }] = useQuery({
+      query: getLastValue,
+      variables: { metricName }
+    })
+
+    if(fetching) {
+      return 'Loading...';
+    } else if(error) {
+      return `This is not the error you are looking for...
+              ERROR: ${error}`
+    }
+
+    const { value } = data.getLastKnownMeasurement
+
+
+    const handleClick = () => {
+      setCardData(value);
+      setToggleChart(!toggleChart);
+      props.viewGraph(toggleChart, props.title)
+    }
+    if(cardData !== null) {
+      console.log(cardData)
+    }
+
+    // #TODO: Need to get the names to toggle view for the value
+
     return (
-        <Card className={classes.card} raised={true}>
+        <Card className={classes.card} raised={true} onClick={() => handleClick()}>
             <CardContent>
                 <Typography className={classes.title}>
-                    { props.title }
+                    {props.title}
                 </Typography>
+                {cardData !== null ? null : <p>{ cardData }</p>}
             </CardContent>
         </Card>
     )
+}
+
+const mstp = state => {
+  return {
+      measurements: state.data,
+      loading: state.loading
+  }
 }
 
 export default MetricsCard
