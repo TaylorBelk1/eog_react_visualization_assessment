33d5c943ff17688811de342ba93b516fa4e388a4
tbelk12 <taylor.belk@americannational.com>
2019-10-07 10:47:26 -0500

    set MetricCardView to determine the tab data it needs to subscribe to. Introduced a useSelector hook to automatically pull updated data from redux store

--- src/components/metrics/MetricsCardView.jsx
+++ src/components/metrics/MetricsCardView.jsx
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import { getMultipleMeasurements } from '../queryStrings';
-import { subtractMinutes } from '../../store/utils';
-import { useQuery } from 'urql';
+import { useSelector } from 'react-redux';
+
+
+const getTabData = state => {
+    const {
+        tubingPressure,
+        flareTemp,
+        injValveOpen,
+        oilTemp,
+        casingPressure,
+        waterTemp
+    } = state.measurementReducer.data;
+    return {
+        tubingPressure,
+        flareTemp,
+        injValveOpen,
+        oilTemp,
+        casingPressure,
+        waterTemp
+    }
+}
 
 const MetricsCardView = (props) => {
-    const [startTime, setStartTime] = useState();
+    const [tab, setTab] = useState();
+    const {
+        tubingPressure,
+        flareTemp,
+        injValveOpen,
+        oilTemp,
+        casingPressure,
+        waterTemp
+    } = useSelector(getTabData);
 
     useEffect(() => {
-        setStartTime(subtractMinutes(Date.now()));
-    }, [])
-
-    const measurements = [
-        {
-          metricName: "tubingPressure",
-          after: startTime,
-        },
-        {
-          metricName: "flareTemp",
-          after: startTime,
-        },
-        {
-          metricName: "injValveOpen",
-          after: startTime,
-        },
-        {
-          metricName: "oilTemp",
-          after: startTime,
-        },
-        {
-          metricName: "casingPressure",
-          after: startTime,
-        },
-        {
-          metricName: "waterTemp",
-          after: startTime,
+        switch(props.title) {
+            case "tubingPressure":
+                setTab(tubingPressure);
+                break;
+            case "flareTemp":
+                setTab(flareTemp);
+                break;
+            case "injValveOpen":
+                setTab(injValveOpen);
+                break;
+            case "oilTemp":
+                setTab(oilTemp);
+                break;
+            case "casingPressure":
+                setTab(casingPressure);
+                break;
+            case "waterTemp":
+                setTab(waterTemp);
+                break;
+            default:
+                setTab(null);
+                break;
         }
-    ]
-
-    const [{ fetching, data, error }] = useQuery({
-        query: getMultipleMeasurements,
-        variables: { measurements }
-    });
-
-    if(fetching) {
-        console.log('fetching..')
-        return 'Loading....'
-    } else if(error) {
-        return 'Error...'
-    }
-
-    props.setInitValues(data.getMultipleMeasurements);
+    }, [
+        props.title,
+        tubingPressure,
+        flareTemp,
+        injValveOpen,
+        oilTemp,
+        casingPressure,
+        waterTemp
+    ])
 
     return (
-        <p>Test</p>
+        <div>
+            {tab && tab[tab.length -1].value}
+        </div>
     )
 }
 
