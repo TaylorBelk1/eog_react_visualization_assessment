14cdffd04bcfedbbc459093f7bb27676d580bf01
tbelk12 <taylor.belk@americannational.com>
2019-10-08 15:06:33 -0500

    added styles to MetricCard

--- src/styled-components/metricCardStyles.js
+++ src/styled-components/metricCardStyles.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
-export const MetricsWrap = styled.div`
-    width: 70%;
+export const MetricCardsWrap = styled.div`
+    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     margin: 40px auto;
+`
+
+export const MetricsWrap = styled.div`
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
 `
