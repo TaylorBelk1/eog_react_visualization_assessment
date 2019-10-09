5564d8f358e605cc8f455f7870bacbdfe6e8e804
tbelk12 <taylor.belk@americannational.com>
2019-10-09 08:31:44 -0500

    created loading component and added it to BodyWrapper

---
+++ src/components/Loading.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { LoadingWrap } from '../styled-components/loadingStyles';
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2),
+  },
+}));
+
+const Loading = () => {
+  const classes = useStyles();
+
+  return (
+    <LoadingWrap>
+      <CircularProgress className={classes.progress} />
+    </LoadingWrap>
+  );
+}
+
+export default Loading
